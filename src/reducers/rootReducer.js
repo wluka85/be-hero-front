7ae28eef38fbe302ba7ba4bbe96feaafd804fe40
wluka85 <wluka85@gmail.com>
2019-01-21 19:42:50 +0100

    Implement rootReducer

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+import accountReducer from "./accountReducer";
+
+const rootReducer = combineReducers({
+    accountReducer
+
+});
+
+export default rootReducer;
