7f8f9674a290db2223c660e327fa25fcae80300c
wluka85 <wluka85@gmail.com>
2019-01-21 19:43:11 +0100

    Add component to main app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import {applyMiddleware, createStore} from 'redux';
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+
+ReactDOM.render(
+    <Provider store={store}>
+        {/*<BrowserRouter>*/}
+            {/*<Switch>*/}
+                {/*<Route exact path='/' component={App} />*/}
+                {/*<Route exact path='/log' component={AppLoggedIn} />*/}
+            {/*</Switch>*/}
+        {/*</BrowserRouter>*/}
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 serviceWorker.unregister();
