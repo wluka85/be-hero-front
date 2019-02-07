b6111aafa0165b9fbff7d772a3a53d4b9f299b39
wluka85 <wluka85@gmail.com>
2019-02-07 10:38:15 +0100

    Add Acount and Registration component to container

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Account from './components/account';
+import MessageAlert from './components/messageAlert';
+import Registration from "./components/registration";
+import './App.css'
 
 class App extends Component {
   render() {
     return (
         <div className="container">
           <Account/>
+          <Registration/>
+          <MessageAlert/>
         </div>
 
       );
