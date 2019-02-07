643898d0ce86863e80985786ca375fe3181b53d7
wluka85 <wluka85@gmail.com>
2019-02-07 10:32:43 +0100

    Implement Hero class

---
+++ src/model/hero.js
@@ -0,0 +1,9 @@
+export class Hero {
+    constructor(name, surname, description, sessionId, level, id) {
+        this.name = name;
+        this.surname = surname;
+        this.description = description;
+        this.level = level;
+        this.id = id;
+    }
+}
