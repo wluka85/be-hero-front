3ff7027645da9e007c23b1832afb00c0147bd35f
wluka85 <wluka85@gmail.com>
2019-02-07 10:31:24 +0100

    Add setting of communication with server (backend)

--- src/actions/apiQueries.js
+++ src/actions/apiQueries.js
@@ -1,3 +1,4 @@
-const url = 'localhost:3000/';
+const url = 'http://localhost:4000/';
 
-export const loginQuery = url + '';
+export const loginQuery = url + 'signin';
+export const registerQuery = url + 'signup';
