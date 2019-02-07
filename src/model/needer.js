f41a5b25fcf6869e0b709f0c6a4205e64d385e3f
wluka85 <wluka85@gmail.com>
2019-02-07 10:33:09 +0100

    Implement Needer class

---
+++ src/model/needer.js
@@ -0,0 +1,8 @@
+export class Needer {
+    constructor(name, surname, description, sessionId, id) {
+        this.name = name;
+        this.surname = surname;
+        this.description = description;
+        this.id = id;
+    }
+}
