4cce2599c91884aef47551f8792533d915b75f51
wluka85 <wluka85@gmail.com>
2019-02-10 10:20:47 +0100

    Implement Router in app

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import Account from './components/account';
-import MessageAlert from './components/messageAlert';
-import Registration from "./components/registration";
 import './App.css'
+import { HeroContainer } from './components/heroContainer';
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
+      <Router>
         <div className="container">
-          <Account/>
-          <Registration/>
-          <MessageAlert/>
+          <Route exact path="/" component={Account} />
+          <Route exact path="/hero" component={HeroContainer} />
+          <Route exact path="/needer" component={HeroContainer} />
         </div>
-
+      </Router>
       );
   }
 }
