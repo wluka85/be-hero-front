0af319961409f660a6ac90d4cafa6d578a33ca31
wluka85 <wluka85@gmail.com>
2019-02-12 12:33:45 +0100

    Add heroSelfCasesQuery

--- src/actions/apiQueries.js
+++ src/actions/apiQueries.js
@@ -2,3 +2,4 @@ const url = 'http://localhost:4000/';
 
 export const loginQuery = url + 'signin';
 export const registerQuery = url + 'signup';
+export const heroSelfCasesQuery = url + 'hero-main/my-cases';
