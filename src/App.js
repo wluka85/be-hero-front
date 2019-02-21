b04307f783750b90e0330266805f2e8ffc8e49df
wluka85 <wluka85@gmail.com>
2019-02-21 11:21:20 +0100

    Implement MuiThemeProvider in App.js module

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import Account from './components/account';
 import './App.css'
-import { HeroContainer } from './components/heroContainer';
+import MainContainer from './components/mainContainer';
 import {
   BrowserRouter as Router,
   Route,
 } from 'react-router-dom';
+import { MuiThemeProvider , createMuiTheme } from '@material-ui/core';
+
+const theme = createMuiTheme({
+  typography: {
+    fontSize: 20
+  }
+});
 
 class App extends Component {
   render() {
     return (
+      <MuiThemeProvider theme={ theme }>
       <Router>
         <div className="container">
           <Route exact path="/" component={Account} />
-          <Route exact path="/hero" component={HeroContainer} />
-          <Route exact path="/needer" component={HeroContainer} />
+          <Route exact path="/hero" component={MainContainer} />
+          <Route exact path="/needer" component={MainContainer} />
         </div>
       </Router>
+      </MuiThemeProvider>
       );
   }
 }
