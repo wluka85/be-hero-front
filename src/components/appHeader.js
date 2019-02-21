13db1b4bfc3a5e4ebfa61f078cd74aef7282a682
wluka85 <wluka85@gmail.com>
2019-02-21 10:47:49 +0100

    Changes bootstrap styles to material-ui style of header component

--- src/components/appHeader.js
+++ src/components/appHeader.js
@@ -1,19 +1,68 @@
 import React, { Component } from 'react';
-import { Navbar, NavItem, Nav } from 'react-bootstrap';
 import { connect } from "react-redux";
 import { handleChangeSidebarOpen } from "../actions/sidebarActions";
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+
+const drawerWidth = 300;
+
+const styles = theme => ({
+    root: {
+      display: 'flex',
+    },
+    drawer: {
+      [theme.breakpoints.up('sm')]: {
+        width: drawerWidth,
+        flexShrink: 0,
+      },
+    },
+    appBar: {
+      marginLeft: drawerWidth,
+      [theme.breakpoints.up('sm')]: {
+        width: `calc(100% - ${drawerWidth}px)`,
+      },
+    },
+    menuButton: {
+      marginRight: 20,
+      [theme.breakpoints.up('sm')]: {
+        display: 'none',
+      },
+    },
+    toolbar: theme.mixins.toolbar,
+    drawerPaper: {
+      width: drawerWidth,
+    },
+    content: {
+      flexGrow: 1,
+      padding: theme.spacing.unit * 3,
+    },
+  });
 
 export class AppHeader extends Component {
     render () {
         const { handleOpenClose } = this.props;
+        const { classes } = this.props;
+
         return (
-            <Navbar fixedTop={true} fluid={true}>
-                <Nav navbar pullLeft={true}>
-                    <NavItem>
-                        <i className="fa fa-bars" onClick={ () => { handleOpenClose() } } style={{ fontSize: '24px' }}></i>
-                    </NavItem>
-                </Nav>
-            </Navbar>
+            <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Open drawer"
+                onClick={handleOpenClose}
+                className={classes.menuButton}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" color="inherit" noWrap>
+                BeHero
+              </Typography>
+            </Toolbar>
+          </AppBar>
         )
     }
 }
@@ -27,8 +76,7 @@ const mapStateToProps = (state) => {
   const mapDispatchToProps = (dispatch) => {
     return {
         handleOpenClose: () => { dispatch(handleChangeSidebarOpen()) },
-
     }
   };
   
-  export default AppHeader = connect(mapStateToProps, mapDispatchToProps)(AppHeader);
+  export default AppHeader = connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(AppHeader));
