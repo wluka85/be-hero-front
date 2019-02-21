1752ef4a9a1e1c5e23c4213869c2a6343e1b02af
Senseinvader <d.ivantsov1983@gmail.com>
2019-02-21 16:35:06 +0100

    Add redxStore support to app by embedding it to the store

--- src/index.js
+++ src/index.js
@@ -4,23 +4,18 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from "react-redux";
-import {applyMiddleware, createStore} from 'redux';
+import {applyMiddleware, createStore, compose} from 'redux';
 import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 import createSocketIoMiddleware from 'redux-socket.io';
 import io from 'socket.io-client';
 let socket = io('http://localhost:4000');
 
-const store = createStore(rootReducer, applyMiddleware(thunk, createSocketIoMiddleware(socket, "server/")));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk, createSocketIoMiddleware(socket, "server/"))));
 
 ReactDOM.render(
     <Provider store={store}>
-        {/*<BrowserRouter>*/}
-            {/*<Switch>*/}
-                {/*<Route exact path='/' component={App} />*/}
-                {/*<Route exact path='/log' component={AppLoggedIn} />*/}
-            {/*</Switch>*/}
-        {/*</BrowserRouter>*/}
         <App/>
     </Provider>
     , document.getElementById('root'));
