2da98fda896009f4275496498685253e78e34d4a
wluka85 <wluka85@gmail.com>
2019-03-07 14:21:17 +0100

    Implement sending message to server in socketActions module

--- src/actions/socketActions.js
+++ src/actions/socketActions.js
@@ -2,3 +2,19 @@ export const sendUserConnectedMessage = (user) => ({
     type: 'server/user-connected',
     user: user
 });
+
+const chatMessageSent = (message) => ({
+    type: 'server/message-sent',
+    message: message
+});
+
+export const sendChatMessage = (message) => (dispatch, getState) => {
+    const content = {
+        contents: message, 
+        // sender: sender,
+        // reciever: reciever,
+        author: getState().accountReducer.user.name,
+        caseId: getState().casesReducer.currentChatCase._id
+    }
+    dispatch(chatMessageSent(content))
+};
