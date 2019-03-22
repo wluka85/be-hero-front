4c1fe115181a03e494ad1170763ca3c5883591f2
Senseinvader <d.ivantsov1983@gmail.com>
2019-03-22 16:16:35 +0100

    Add needer self cases query path to apiQueries module

--- src/actions/apiQueries.js
+++ src/actions/apiQueries.js
@@ -3,6 +3,7 @@ export const url = 'http://localhost:4000/';
 export const loginQuery = url + 'signin';
 export const registerQuery = url + 'signup';
 export const heroSelfCasesQuery = url + 'hero-main/my-cases';
+export const neederSelfCasesQuery = url + 'needer-main/my-cases';
 export const getCasesQuery = (role) => {
     return url + role + '-main/my-cases/';
 }
