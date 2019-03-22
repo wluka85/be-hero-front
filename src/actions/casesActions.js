741dd615d9a9aecaf3e2205579ae4a90679fc0eb
wluka85 <wluka85@gmail.com>
2019-03-22 14:59:26 +0100

    Hotfix in casesActions

--- src/actions/casesActions.js
+++ src/actions/casesActions.js
@@ -24,3 +24,24 @@ export const setActiveCaseCurrentChat = (activeCaseId) => (dispatch, getState) =
     })
     .catch(error => console.log(error));
 }
+
+export const fetchChoosenFreeCase = (caseId) => (dispatch, getState) => {
+    let role = getState().accountReducer.role;
+    let accessToken = localStorage.getItem('accessToken');
+    fetch(getCasesQuery(role) + caseId, {
+        method: 'GET',
+        headers: {'Accept': 'application/json', 
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + accessToken,
+            'params': {
+                caseId: caseId
+            }
+        },
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        // dispatch(activeCaseCurrentChatSet(data.case[0]));
+    })
+    .catch(error => console.log(error));
+}
