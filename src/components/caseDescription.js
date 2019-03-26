34abff563b2352639cac94b6126fc051c4d7c894
wluka85 <wluka85@gmail.com>
2019-03-26 16:10:33 +0100

    Change view of caseDescription (add personal data of needer)

--- src/components/caseDescription.js
+++ src/components/caseDescription.js
@@ -21,14 +21,37 @@ const styles = {
     padding: 30
   },
   cardContent: {
+    // padding: 150,
     display: 'flex',
     alignItems: 'center',
+    justifyContent: 'center',
     flexDirection: 'column',
+  },
+  labelsContainer: {
+    padding: 10,
+    textAlign: 'right'
+  },
+  infoContainer: {
+    padding: 10,
+    textAlign: 'left'
   },
   buttonArea: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around'
+  },
+  item: {
+    display: 'flex',
+    flexDirection: 'row',
+    width: '100%'
+  },
+  itemDescription: {
+    width: '100%',
+  },
+  itemContent: {
+    width: '80%',
+    textAlign: 'right',
+    marginRight: '10px'
   }
 };
 
@@ -45,15 +68,38 @@ class CaseDescription extends React.Component {
     return (
       <Card className={classes.card}>
           <CardContent className={classes.cardContent}>
-            <Typography gutterBottom variant="h5" component="h2">
-              { chosenCase.description }
-            </Typography>
-            <Typography component="p">
-              author: { chosenCase.neederLogin }
-            </Typography>
-            <Typography component="p">
-              date: { moment(chosenCase.timeStamp).format('lll') }
-            </Typography>
+            <div className={classes.item}>
+              <Typography className={classes.itemContent} gutterBottom variant="h5" component="h2">
+                description:
+              </Typography>
+              <Typography className={classes.itemDescription} gutterBottom variant="h5" component="h2">
+                { chosenCase.description }
+              </Typography>
+            </div>
+            <div className={classes.item}>
+              <Typography className={classes.itemContent} variant="h6" component="p">
+                author:
+              </Typography>
+              <Typography className={classes.itemDescription} variant="h6" component="p">
+                { chosenCase.neederLogin }
+              </Typography>
+              </div>
+            <div className={classes.item}>
+              <Typography className={classes.itemContent} variant="h6" component="p">
+                personal data:
+              </Typography>
+              <Typography className={classes.itemDescription} variant="h6" component="p">
+                { chosenCase.personalData ? chosenCase.personalData : '-' }
+              </Typography>
+            </div>
+            <div className={classes.item}>
+              <Typography className={classes.itemContent} variant="h6" component="p">
+                date:
+              </Typography>
+              <Typography className={classes.itemDescription} variant="h6" component="p">
+                { moment(chosenCase.timeStamp).format('lll') }
+              </Typography>
+            </div>
           </CardContent>
         <CardActions className={classes.buttonArea}>
           <Button size="large" color="primary" onClick={() => { history.goBack() }}>
