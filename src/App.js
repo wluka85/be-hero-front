779aba66ba3011167fbe00d83ab0e430e6c82311
Senseinvader <d.ivantsov1983@gmail.com>
2019-03-27 16:10:05 +0100

    Change color scheme in theme object

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import {
   Route,
 } from 'react-router-dom';
 import { MuiThemeProvider , createMuiTheme } from '@material-ui/core';
+import pink from '@material-ui/core/colors/pink';
+import teal from '@material-ui/core/colors/teal';
+import red from '@material-ui/core/colors/red';
 
 const theme = createMuiTheme({
   typography: {
     fontSize: 20
-  }
+  },
+  palette: {
+    primary: teal,
+    secondary: pink,
+    error: red,
+    contrastThreshold: 3,
+    tonalOffset: 0.2,
+  },
 });
 
 class App extends Component {
